c2f0c74da1756cb5a9d238c652695d08c6e21097
Osama-Elsamny <oelsamny@sfu.ca>
2018-10-20 15:44:51 -0700

    Update secondCount.c
    Now the multi-threading function is working, also the ouput function is working. So the program would be working just fine, but the printed list would be sorted, also need to work on freeing the memory

--- secondCount.c
+++ secondCount.c
@@ -8,7 +8,7 @@
 #include "string.h"
 
 #define THREAD_NUM 5
-#define MAX_NUM_WORD 40
+#define MAX_NUM_WORD 1024
 int size;
 typedef struct Map{
     char* key;
@@ -22,6 +22,13 @@ typedef struct{
     int endIndx;
     Map *mylist;
 }Args;
+typedef struct{
+    int threadNum;
+    int startIndx;
+    int endIndx;
+    Map *myOutput;
+    Map **mylist;
+}ArgsReduce;
 
 /** 
  * Funcition name: input_reader
@@ -45,19 +52,6 @@ void input_reader(char* file, char** word){
     }
     size = i;
     fclose(inputFile);
-}
-
-/** 
- * Funcition name:
- * Purpose: 
- * Developer:
- * Input: 
- * Output: 
-*/
-void print(char** word, int arrsize){
-    for(int i = 0; i < arrsize; i++){
-        printf("%s\n", word[i]);
-    }
 }
 
 /** 
@@ -153,13 +147,34 @@ void* ThreadMap(void* arg){
 }
 
 /** 
- * Funcition name:
- * Purpose: 
- * Developer:
- * Input: 
- * Output: 
+ * Funcition name: initializeThreadMappingInput
+ * Purpose: To initialize multiple inputs and outputs for 
+ *          the thread function
+ * Developers: Osama Elsamny and Jasmandeep Batra
+ * Input: to be completed -------------------------
+ * Output: N/A
 */ 
-void initializeThreadInput(Args **input, char** key,int n){
+void initializeThreadReduceInput(ArgsReduce **input2, Map **list, int n){
+    for(int i = 0; i < THREAD_NUM; i++){
+        input2[i] = (ArgsReduce*) malloc(sizeof(ArgsReduce));
+        input2[i]->threadNum = i;
+        input2[i]->startIndx = floor( i * n/THREAD_NUM);
+        input2[i]->endIndx = floor((i+1) * n/THREAD_NUM);
+        input2[i]->mylist = list;
+        input2[i]->myOutput = NULL;
+    }
+}
+
+/** 
+ * Funcition name: initializeThreadMappingInput
+ * Purpose: To initialize multiple inputs and outputs for 
+ *          the thread function
+ * Developers: Osama Elsamny and Jasmandeep Batra
+ * Input: the whole array, the size of the array, pointer to
+ *        the arguments array
+ * Output: N/A
+*/ 
+void initializeThreadMappingInput(Args **input, char** key,int n){
     for(int i = 0; i < THREAD_NUM; i++){
         input[i] = (Args*) malloc(sizeof(Args));
         input[i]->startIndx = floor( i * n/THREAD_NUM);
@@ -168,6 +183,102 @@ void initializeThreadInput(Args **input, char** key,int n){
         input[i]->array = key;
         input[i]->mylist = NULL;
     }
+}
+
+/** 
+ * Funcition name: combineList
+ * Purpose: To combine the list after is done mapping
+ * Developer: Osama Elsamny
+ * Input: The argument array that had the output saved 
+ *        after the threads are done with the computations
+ * Output: Map pointer to the list
+*/ 
+Map* combineList(Args **input){
+    Map *temp = NULL;
+    temp = input[0]->mylist;
+    for(int i = 1; i < THREAD_NUM; i++){
+        Map *itr = NULL;
+        itr = input[i-1]->mylist;
+        while(itr->next != NULL){
+            itr = itr->next;
+        }
+        itr->next = input[i]->mylist;
+    }
+    return temp;
+}
+//-----add comment
+int getsize(Map *list){
+    int n = 1;
+    for(Map *itr = list; itr->next != NULL; itr = itr->next){
+        if(strcmp(itr->key, itr->next->key) != 0){
+            n++;
+        }
+    }
+    return n;
+}
+
+/** 
+ * Funcition name: findSimilarList
+ * Purpose: To seperate each key list in a different 
+ *          into a different list
+ * Developer: Osama Elsamny
+ * Input: The whole maped list 
+ * Output: A pointer to each pointer of different list
+*/
+void findSimilarList(Map **temp, Map *list, int n){
+    for(int i = 0; i < n; i++){
+        temp[i] = (Map*) malloc(sizeof(Map));
+    }
+    Map *itr = list;
+    Map *itr2 = list;
+    int i = 1;
+    temp[0] = list;
+    while(itr2 != NULL){
+        if(itr->next == NULL){
+            temp[i] = NULL;
+            break;
+        }
+        while(strcmp(itr->key, itr->next->key) == 0){
+            itr = itr->next;
+            itr2 = itr2->next;
+            if(itr->next == NULL)
+                break;
+        }
+        if(itr->next == NULL){
+            temp[i] = NULL;
+            break;
+        }
+        itr2 = itr2->next;
+        itr->next = NULL;
+        itr = itr2;
+        temp[i] = itr;
+        i++;
+    }
+}
+
+/** 
+ * Funcition name: ThreadReduce
+ * Purpose: To manage multiple threads calling the reduce 
+ *          function 
+ * Developer: Osama Elsamny
+ * Input: 
+ * Output: N/A
+*/ 
+void* ThreadReduce(void* arg){
+    ArgsReduce *input = (ArgsReduce *) arg;
+    Map *temp = NULL;
+    for(int i = input->startIndx; i < input->endIndx; i++){
+        if(input->myOutput == NULL){
+            input->myOutput = reduce(input->mylist[i]);
+            temp = input->myOutput;
+            input->threadNum = input->threadNum + 1;
+            continue;
+        }
+        temp->next = reduce(input->mylist[i]);
+        temp = temp->next;
+        input->threadNum = input->threadNum + 1;
+    }
+    return NULL;
 }
 
 /** 
@@ -180,28 +291,44 @@ void initializeThreadInput(Args **input, char** key,int n){
  * Output: ouput the wordCount
 */
 int main(int argc, char* argv[]){
-    char* word[MAX_NUM_WORD];
-    Args *theadsInput[THREAD_NUM];
+    char *word[MAX_NUM_WORD];
+    Args *theadsMappingInput[THREAD_NUM];
+    ArgsReduce *theadsReduceInput[THREAD_NUM];
     pthread_t threadMapping[THREAD_NUM];
+    pthread_t threadReducing[THREAD_NUM];
     int localsize = 0;
+    Map *combinedList = NULL;
+    int wordNum = 0;
     size = 0;
 
     input_reader(argv[1], word);
     localsize = size;
     sort(word, localsize);
-    initializeThreadInput(theadsInput, word, localsize);
+    initializeThreadMappingInput(theadsMappingInput, word, localsize);
     for(int i = 0; i < THREAD_NUM; i++){
-        pthread_create(&threadMapping[i], NULL, ThreadMap, (void *) theadsInput[i]);
+        pthread_create(&threadMapping[i], NULL, ThreadMap, (void *) theadsMappingInput[i]);
     }
     for(int i = 0; i < THREAD_NUM; i++){
         pthread_join(threadMapping[i], NULL);
     }
+    combinedList = combineList(theadsMappingInput);
+    wordNum = getsize(combinedList);
+    Map *similarList[wordNum];
+    findSimilarList(similarList, combinedList, wordNum);
+    initializeThreadReduceInput(theadsReduceInput, similarList,wordNum);
     for(int i = 0; i < THREAD_NUM; i++){
-        for(Map *itr = theadsInput[i]->mylist; itr != NULL; itr = itr->next){
-            output(itr);
+        pthread_create(&threadReducing[i], NULL, ThreadReduce, (void *) theadsReduceInput[i]);
+    }
+    for(int i = 0; i < THREAD_NUM; i++){
+        pthread_join(threadReducing[i], NULL);
+    }
+    for(int i = 0; i < THREAD_NUM; i++){
+        Map *temp = theadsReduceInput[i]->myOutput;
+        while(temp != NULL){
+            output(temp);
+            temp = temp->next;
         }
     }
-    //output(reduce(mylist));
     return 0;
 }
 /** 
